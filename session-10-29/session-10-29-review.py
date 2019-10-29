0fa4daac97f7e34894d49cf10289c7fedb13db2b
Gladys Porfirio <gporfirio011@gmail.com>
2019-10-29 17:56:32 -0400

    Completed Challenges

--- session-10-29/session-10-29-review.py
+++ session-10-29/session-10-29-review.py
@@ -1,23 +1,36 @@
 # Welcome back!
-
+import numpy as np
 # Reviewing While loops:
+# print a while loop that prints 1-12
+
+i = 0
+
+while i < 13:
+	print(i)
+	i+=1
+
 # Write code to print multiples of 5 from 45 to 90
 
+i = 9
+while i < 19:
+	print(5*i)
+	i += 1
 
 # With your partner: discuss: What is a set? What is it good for?
 # Write your response as a comment here:
-#
+# A set is an unordered that stores data and is good for storing data
 
 # Create a variable called "myset" and put whatever you want in it:
 
+myset = ['miss', 'cocoa', 'hot', 'mind', 'vibin', 'dime','sweater']
 
 
-# Create a 2D numpy array of size 3x5 with numbers 1-5:
+# Create a 2D numpy array of size 3x5 with numbers 1-15:
 
-
+a = np.array([(1, 2, 3, 4, 5), (6, 7, 8, 9, 10), (11, 12, 13, 14, 15)])
 
 # From your array, print the number 7:
-
+print(a[1,1])
 
 
 # Challenge: use while loop to print the following pattern:
@@ -25,6 +38,13 @@
 # ***
 # **
 # *
+i = 0
+while i < '*****':
+ 	print(i)
+ 	i += '*'
+
+ i = ['****','***','**','**','*']
+print(i)
 
 
 
