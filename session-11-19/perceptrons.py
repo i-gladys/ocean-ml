2f0c5bce00b46aad4390d1b479306f0a7ef1176a
Gladys Porfirio <gporfirio011@gmail.com>
2019-11-21 18:17:26 -0500

    changes

--- session-11-19/perceptrons.py
+++ session-11-19/perceptrons.py
@@ -2,28 +2,30 @@ import numpy as np
 
 # Pick a decision you want to make. Make sure you pick a binary decision,
 # meaning the answer is either yes or no: 
-decision = "your - decision - here"
+decision = "should I start a dance team?"
 
 # Pick three factors that affect your decision and write them as strings
-factor_1 = "write - factor - 1 "
-factor_2 = "write - factor - 2"
-factor_3 = "write - factor - 3"
+factor_1 = "Do I have time to lead the team?"
+factor_2 = "Will it look on your resume?"
+factor_3 = "How many people would join the team?"
 
 # think about how much each of these factors matter
 # create a numpy array of length 3 where the first
 # value is how much the first factor matters, the 
 # second value is how much the second factor matters
 # etc:
-weights = #your code here
+#your code here
+weights = np.array([10,8,9])
+
 
 # pick a value for the bias: explain to your partner what this means
-bias = -90 # change this
+bias = 10 # change this
 
 # now create a numpy array that says if the factors are true or not.
 # Use 1 = true, 0 = false
 # for example, if your fist factor is "rain", and it's not raining
 # the first value in your array will be 0
-factor_values = # your code here
+factor_values = np.array([0,1,0])
 
 # now the perceptron is going to calculate the outcome. Make sure you understand
 # what this code is and what it does: 
@@ -48,7 +50,7 @@ print("*************************")
 print("weighted sum = "+str(output_1))
 print("final decision: "+ test_dict[output])
 
-# print(output_1)
+print(output_1)
 
 
 
--- session-11-19/sigmoid.py
+++ session-11-19/sigmoid.py
@@ -4,22 +4,22 @@ import numpy as np
 # you are chosing between two things
 # Example: "Should I buy white sneakers instead of black sneakers?"
 # or "Should I take AP chemistry?"  
-decision = "your - decision - here"
+decision = "Should I ask for a camera this christmas?"
 
 # Pick three factors that affect your decision and write them as strings
-factor_1 = "factor - 1 "
-factor_2 = "factor - 2"
-factor_3 = "factor - 3"
+factor_1 = "Is it more valuable than a keyboard?"
+factor_2 = "Is it useful?"
+factor_3 = "It is neccessary?"
 
 # think about how much each of these factors matter
 # create a numpy array of length 3 where the first
 # value is how much the first factor matters, the 
 # second value is how much the second factor matters
 # etc:
-weights = #your code here
+weights = np.array([8,7,7]) #your code here
 
 # pick a value for the bias: explain to your partner what this means
-bias = -9 # change this
+bias = 10 # change this
 # keep your weights and biases on the same scale
 # for example if your weights are 6, 7, 9 don't make your bias 100
 
@@ -29,7 +29,7 @@ bias = -9 # change this
 # but you think that probably won't happen, the first value in  
 # your array might be 0.10, meaning you think you are 10% likely to 
 # get your shoes dirty
-factor_values = # your code here
+factor_values = np.array([0.30,0.70,0.60])# your code here
 
 # now the perceptron is going to calculate the outcome. This is
 # basically the same as the perceptron example except for the 
