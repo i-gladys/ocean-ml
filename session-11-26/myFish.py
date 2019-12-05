c60d9bb059b78a3eeda40cc98e13f3b89153fcbc
Gladys Porfirio <gporfirio011@gmail.com>
2019-12-05 17:03:24 -0500

    december 5

--- session-11-26/myFish.py
+++ session-11-26/myFish.py
@@ -1,14 +1,21 @@
 class Fish:
 	def __init__(self, name='Fish',hunger=0,happiness = 10):
-		self.name = 'name your fish' 
+		self.name = 'paco' 
 		self.hunger = hunger
 		self.happiness = happiness
 		# add 4 other data fields 
+		self.height = height
+		self.weight = weight
+		self.
 
 	def eat(self):
 		# what happens hunger and happiness when the fish eats?
-
+		self.hunger = self.hunger- 1
+		self.happiness = self.happiness+1
+		print(self.name + " has eaten.")
 	def swim(self):
 		# what happens to hunger and happiness when the fist swims?
-
+		self.hunger= self.hunger+1
+		self.happiness
 	#what other things does the fish do besides eat and swim?
+fish = Fish()
