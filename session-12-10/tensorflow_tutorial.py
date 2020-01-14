fa7e634e21062db8ad85df0ef7290b7dd5953b9e
Gladys Porfirio <gporfirio011@gmail.com>
2020-01-14 17:46:09 -0500

    change 1/14/20

--- session-12-10/tensorflow_tutorial.py
+++ session-12-10/tensorflow_tutorial.py
@@ -58,14 +58,13 @@ history = model.fit(train_images, train_labels, epochs=10,
                     validation_data=(test_images, test_labels))
 
 #Translate the loss of the data set after it is runned
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
-plt.legend(loc='lower right')
+#plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+#plt.xlabel('Epoch')
+#plt.ylabel('Accuracy')
+#plt.ylim([0.5, 1])
+#plt.legend(loc='lower right')
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+#test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
 #plots the data set after finding out its loss and updating its accuracy
 
